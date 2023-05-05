ba8bad22032045a3f82efc07212459d53b52d4cc
ROMER Pierre-Elie <pierre.romer@adn.fr>
2023-05-05 11:19:18 +0200

    optimisation lancement de l'app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { run, upAll } from 'docker-compose';
+import { v2 as compose } from 'docker-compose';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
